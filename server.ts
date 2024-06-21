8105145bcc92fc5f45b55624f3c2c5604610be3e
Pratik Jadhav <prateek7120@gmail.com>
2024-06-21 14:44:08 +0530

    delete endpoint added

--- server.ts
+++ server.ts
@@ -20,7 +20,11 @@ if (!fs.existsSync(dbFile)) {
 //configure express to received form data
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.get('/ping', (request:express.Request, response:express.Response)=>{
     response.status(200).json({ result: true });
 });
@@ -66,27 +70,61 @@ app.post('/submit',(request:express.Request, response:express.Response)=>{
 //read endpoint to return json obj at that given index in responce
 
 app.get('/read',(request:express.Request, response:express.Response)=>{
-    const index:number =parseInt(request.body.index);
+  console.log("step 1 pass");
+  //const idx:string= request.query.index;
 
+    //const index:number =parseInt(request.query.index);
+    //const index = typeof request.query.index === 'tring'? parseInt(request.query.index, 10) : undefined;
+    const index = parseInt(request.query.index as string, 10);
+    console.log(request.query.index)
     //checking the  number is int or not
     if(isNaN(index))
         {
             response.status(400).json({ error: 'Invalid index' });
             return;
         }
+    console.log("step 2 pass")
         //reading json file from db.json and storing it  at dbdata which is of any[]  which specifies result 
         //will have values of  'any' type  array  and fs is node js module which have readfilesyc method
         //which reads the entire content of file return buffer decode with utf-8
-        const dbData: any[] = JSON.parse(fs.readFileSync(dbFile, 'utf8'));
-
+        const dbData: any[] = JSON.parse(fs.readFileSync(dbFile, 'utf8'))
+        console.log("step 3 pass")
         if (index >= dbData.length) {
             response.status(404).json({ error: 'Index out of bounds' });
             return;
           }
-        
+          console.log("step 4 pass")
           const dataAtIndex: any = dbData[index];
           response.status(200).json(dataAtIndex);
+          console.log("step 5 pass ")
 });
+
+
+app.get('/delete',(request:express.Request, response:express.Response)=>{
+  console.log("step 1 pass");
+  //const idx:string= request.query.index;
+
+    //const index:number =parseInt(request.query.index);
+    //const index = typeof request.query.index === 'tring'? parseInt(request.query.index, 10) : undefined;
+    const index = parseInt(request.query.index as string, 10);
+    console.log(request.query.index)
+    //checking the  number is int or not
+    if(isNaN(index))
+        {
+            response.status(400).json({ error: 'Invalid index' });
+            return;
+        }
+    console.log("step 2 pass")
+    const data = fs.readFileSync(dbFile);
+    const jsonData = JSON.parse(data.toString());
+
+    jsonData.splice(index, 1);
+    fs.writeFileSync(dbFile, JSON.stringify(jsonData));
+
+    response.status(201).json({ message: 'Form data Deleted successfully' });
+});
+
+
 
 
 
